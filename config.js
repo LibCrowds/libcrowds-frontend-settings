fa86692c0a6dd45bdf06eed7a3a1b084d9e3647b
Alexander Mendes <alexanderhmendes@gmail.com>
2017-08-16 11:44:21 +0100

    Remove core site settings from config

--- config.js
+++ config.js
@@ -3,16 +3,9 @@ const basePath = '/custom/site/settings/projects/playbills'
 
 // Basic configuration
 const config = {
-  brand: 'In the Spotlight',
+  name: 'In the Spotlight',
   tagline: 'Bring past performances from the British Library’s historic playbills collection to life.',
   description: 'Help rediscover popular entertainment from the past 300 years by transcribing names and performances on the playbills of Britain’s old theatres.',
-  company: 'The British Library',
-  contact: {
-    twitter: 'LibCrowds',
-    email: 'crowdsourcing@bl.uk'
-  },
-  githubUrl: 'https://github.com/LibCrowds',
-  pybossaHost: isDev ? 'http://127.0.0.1:5000' : 'https://playbills-backend.libcrowds.com',
   dataLicense: {
     name: 'CC0',
     url: 'https://creativecommons.org/publicdomain/zero/1.0/'
@@ -31,23 +24,6 @@ config.avatarPreference = [
   'custom',
   'gravatar'
 ]
-
-// Mapbox configuration for location stats
-config.mapbox = {
-  id: 'mapbox.streets',
-  publicApiToken: 'pk.eyJ1IjoibGliY3Jvd2RzIiwiYSI6ImNpdmlxaHFzNTAwN3YydHBncHV3dHc3aXgifQ.V4WUx9SDcU_XLFJo2M3RxQ'
-}
-
-// Specify additional parts of the site that require admin rights
-config.requiresAdmin = {
-  projectManagement: true
-}
-
-// Sentry public DSN
-config.sentryPublicDsn = 'https://6b592ec745ca4c7bb8d865e1c002e624@sentry.io/195392'
-
-// Google analytics tracking ID
-config.analytics = 'UA-59584422-4'
 
 // Link to the site forum
 config.forumUrl = 'https://community.libcrowds.com/t/in-the-spotlight'
@@ -75,15 +51,6 @@ config.about = {
       markdown: require(`@${basePath}/about/contact.md`)
     }
   ]
-}
-
-// Vue component for the results page
-// config.resultsComponent = require(`@/${basePath}/Results.vue`)
-
-// Footer lead text
-config.footer = {
-  text: 'Part of the LibCrowds crowdsourcing platform',
-  url: 'http://www.libcrowds.com'
 }
 
 module.exports = config
